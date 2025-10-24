e3eac2f093527f91a0d93fdb679292e7acbc188c
zhudasen <zhudasen@126.com>
2025-10-24 10:39:05 +0800

    1

---
+++ popup/multpromots/multpromots.js
@@ -0,0 +1,21 @@
+// popup.js
+import {
+  initializePopup,
+  setupEventListeners,
+  loadStoredData,
+} from "../popup/popupUtils.js";
+import { setupDragDropEvents } from "../popup/dragDropHandler.js";
+
+document.addEventListener("DOMContentLoaded", function () {
+  // 初始化弹窗
+  initializePopup();
+
+  // 加载存储的数据
+  loadStoredData();
+
+  // 设置所有事件监听器
+  setupEventListeners();
+
+  // 初始化指定输入框的拖放事件
+  setupDragDropEvents();
+});
