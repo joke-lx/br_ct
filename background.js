e7f59c37263f3738a573f5ba4d21f54a38098f1a
zlxmii <2096343460@qq.com>
2025-12-23 21:12:32 +0800

    添加空格

--- background.js
+++ background.js
@@ -9,8 +9,10 @@ import { startServer } from './backgroudtask/word_http_server.js';
 // 启动所有监听器
 setupTabUpdateListener();         // AI 平台标签页更新监听
 setupAIProcessorListener();       // AI 平台消息监听
+
 setupFuncCommandListener();       // 快捷键命令监听
 setupFuncExecutorListener();      // 函数执行消息监听
+
 setTabTransListener();            // 标签页跳转消息监听
 startServer();                    // 单词 HTTP 服务器 (包含消息监听)
 // setupClipboardToFileListener();
